73f19d0a21e4270e4f4e7e0dff2ae80711a772a3
Sigge Brandt <156670844+SB-YG@users.noreply.github.com>
2024-01-15 12:57:28 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>... hello world</h2>
+      <h2>A website in react</h2>
       <img src="https://reactjs.org/logo-og.png" alt="React Image" />
     </div>
   );
