add8d6bd0b4f6aa4eb39690e128777955456560e
Sigge Brandt <156670844+SB-YG@users.noreply.github.com>
2024-01-15 12:41:36 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>... hello</h2>
+      <h2>... hello world</h2>
     </div>
   );
 }
