bb7be32fa1ce2474158df1377ecfd1a8163607dc
Sigge Brandt <156670844+SB-YG@users.noreply.github.com>
2024-01-16 14:07:05 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>A website in react</h2>
-      <img src="https://reactjs.org/logo-og.png" alt="React Image" />
+      <img
+        style={{ width: "50%", height: "50%" }}
+        src="https://reactjs.org/logo-og.png"
+        alt="React Image"
+      />
     </div>
   );
 }
