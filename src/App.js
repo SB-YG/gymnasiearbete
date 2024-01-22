e8dcdbf7bfd24fab124cb0a2d471e6b45d6a88d4
Sigge Brandt <156670844+SB-YG@users.noreply.github.com>
2024-01-22 12:43:52 +0000

    .

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1 style={{ color: "blue" }}>Hello CodeSandbox</h1>
       <h2>A website in react</h2>
       <img
-        style={{ width: "50%", height: "50%" }}
+        style={{
+          width: "50%",
+          height: "50%",
+          backgroundColor: "red",
+          border: "10px solid lightblue",
+        }}
         src="https://www.reactjs.org/logo-og.png"
         alt="React Image"
       />
