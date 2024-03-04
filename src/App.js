b0ae9c6fd02902f2e880e857397a8ff6b026093f
Sigge Brandt <156670844+SB-YG@users.noreply.github.com>
2024-03-04 13:13:58 +0000

    lade till en klickknapp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
   return (
@@ -6,15 +7,12 @@ export default function App() {
       <h1 style={{ color: "green" }}>Gymnasiearbete</h1>
       <h2>A website in react</h2>
       <img
-        style={{
-          width: "50%",
-          height: "50%",
-          backgroundColor: "red",
-          borderRadius: "10px",
-        }}
+        className="image"
         src="https://www.reactjs.org/logo-og.png"
         alt="React Image"
       />
+      <br />
+      <MyButton />
       <ul>
         <li>https://www.figma.com/</li>
         <li>https://www.flaticon.com/</li>
@@ -22,3 +20,28 @@ export default function App() {
     </div>
   );
 }
+
+// functions below;
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return <button onClick={handleClick}>Clicked {count} times</button>;
+}
+
+/*
+      <img
+        style={{
+          className: "image",
+          width: "50%",
+          height: "50%",
+          backgroundColor: "red",
+          borderRadius: "10px",
+        }}
+        src="https://www.reactjs.org/logo-og.png"
+        alt="React Image"
+      />*/
